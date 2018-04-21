3ff29f67817049da70f1c2281e87c2b1a0ffadfd
nurgul nugumanova <31763447+nurgulBeyHive@users.noreply.github.com>
2018-04-21 21:20:54 +0600

    Add files via upload

---
+++ djangocontacts/accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from .forms import UserLoginForm, UserRegistrationForm
+from usercontacts.models import Contact
+
+# Create your views here.
+def get_index(request):
+    if request.user.is_authenticated():
+        contacts = Contact.objects.filter(owner=request.user)
+    else:
+        contacts = []
+    return render(request,'index.html', {'contacts': contacts})
+    
+def logout(request):
+    auth.logout(request)
+    return redirect(get_index)
+    # return render(request, 'index.html')
+    
+def login(request):
+    if request.method=="POST":
+        form=UserLoginForm(request.POST)
+        
+        if form.is_valid():
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password'])
+            if user is not None:
+                auth.login(request, user)
+            else:
+                form.add_error(None, "Your details are not valid")
+            return redirect(get_index)
+    else:
+        form = UserLoginForm()
+    
+    return render(request, "login.html", {'form': form})
+    
+def register(request):
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+    
+        if form.is_valid():
+            user = form.save()
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                             password=form.cleaned_data['password1'])
+            if user is not None:
+                auth.login(request, user)
+                return redirect(get_index)
+                
+    else:
+        form = UserRegistrationForm()
+        
+    return render(request, "register.html", {'form': form})
